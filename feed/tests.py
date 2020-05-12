9c23720984e18ec584aa1398ffa79d51480827f3
Taras <rumezhak.taras@gmail.com>
2020-05-12 15:00:51 +0300

    final commit

--- feed/tests.py
+++ feed/tests.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from feed.models import Post, Like
+
+
+class PostModelTests(TestCase):
+    def test_post_representation(self):
+        """Check the correctness of string representation"""
+        post = Post(title="Test post")
+        self.assertEquals(str(post), "Test post")
--- feed/views.py
+++ feed/views.py
@@ -1,22 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.core.files.storage import FileSystemStorage
-from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404
-
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-
 from feed.forms import CreateBlogPostForm
-from feed.models import Post, upload_location, Like
+from feed.models import Post, Like
 
 
 def index(request):
-    """Home page of the learning log application"""
+    """Home page of the social network"""
     return render(request, "feed/index.html")
 
 
 @login_required(login_url='users:login')
 def posts(request):
+    """Representation of all posts"""
     posts = Post.objects.order_by('-publication_date')
     context = {'posts': posts}
     return render(request, 'feed/posts.html', context)
@@ -24,7 +21,7 @@ def posts(request):
 
 @login_required(login_url='users:login')
 def post(request, post_id):
-    """Renders one topic and all it's entries"""
+    """Representation of one post"""
     post = Post.objects.get(id=post_id)
     # likes = post.entry_set.order_by('-date_added')
     is_liked = False
@@ -33,35 +30,10 @@ def post(request, post_id):
     context = {'post': post, 'is_liked': is_liked, 'likes_count': post.likes_count()}
     return render(request, 'feed/post.html', context)
 
-# @login_required(login_url='users:login')
-# def new_post(request):
-#     """Creates new topic"""
-#     if request.method != 'POST':
-#         # Data is not sent, creates emply form
-#         form = CreateBlogPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             return HttpResponseRedirect(reverse('feed:posts'))
-#     else:
-#         # Sent POST data, process data
-#         form = CreateBlogPostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # new_post = form.save(commit=False)
-#             # new_post.author = request.user
-#             # file = request.FILES[0]
-#             # fs = FileSystemStorage()
-#             # filename = fs.save(file.name, file)
-#             # uploaded_file_url = fs.url(filename)
-#             # new_post.image = uploaded_file_url
-#             # new_post.save()
-#             form = CreateBlogPostForm()
-#             # return HttpResponseRedirect(reverse('feed:posts'))
-#
-#     context = {'form': form}
-#     return render(request, 'feed/new_post.html', context)
-
 
 @login_required(login_url='users:login')
 def new_post(request):
+    """Create a new post"""
     context = {}
     form = CreateBlogPostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
@@ -79,10 +51,9 @@ def new_post(request):
 
 @login_required(login_url='users:login')
 def like_post(request):
+    """Like the post"""
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
     is_liked = False
-    # likes_api = get_object_or_404(Like, post=post)
-    # likes_api = Like.object
     if post.likes.filter(id=request.user.id).exists():
         post.likes.remove(request.user)
         Like.objects.filter(post=post, user=request.user).delete()
